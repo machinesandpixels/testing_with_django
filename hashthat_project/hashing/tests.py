175c4e8156cd82df1a3b717651e8e3919b7155f3
JQ <2016quezadaj@gmail.com>
2020-11-14 22:58:16 -0800

    Expanded Functional Test Case to test hash of hello

--- hashthat_project/hashing/tests.py
+++ hashthat_project/hashing/tests.py
@@ -8,8 +8,14 @@ class FunctionalTestCase(TestCase):
 
     def test_homepage(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('install', self.browser.page_source)
+        self.assertIn('Enter Hash here', self.browser.page_source)
         # assert browser.page_source.find('install')
+
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
 
     def tearDown(self):
         self.browser.quit()
